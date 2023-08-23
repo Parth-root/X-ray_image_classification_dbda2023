5899c9929d3e5029fb0e4b5f815710a9517d09b2
Parth <poproot4@gmail.com>
2023-08-23 13:47:01 +0530

    Update Ui.py

--- Ui/Ui.py
+++ Ui/Ui.py
@@ -41,7 +41,7 @@ def PneumoniaPrediction(img):
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     #st.image(image, caption='Uploaded Image', use_column_width=True)
-    resized_image = image.resize((200, 200))
+    resized_image = image.reshape((200, 200))
     ans=PneumoniaPrediction(resized_image)
    
 
