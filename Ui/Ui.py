fec6fa9d1c9ace544be01bfbbbde22f1d4f0bcba
Parth <poproot4@gmail.com>
2023-08-24 15:16:46 +0530

    Update Ui.py

--- Ui/Ui.py
+++ Ui/Ui.py
@@ -3,51 +3,55 @@ import streamlit as st
 import pickle
 import numpy as np
 import keras
-#import urllib.request
-
-#urllib.request.urlretrieve('https://github.com/Parth-root/X-ray_image_classification_dbda2023/blob/main/Ui/your_model.h5', 'your_model.h5')
-#filename = 'https://github.com/Parth-root/X-ray_image_classification_dbda2023/blob/main/Ui/your_model.h5'
+from PIL import Image
 filename = 'Ui/your_model.h5'
 model = keras.models.load_model(filename)
 
-from PIL import Image
+def PneumoniaPrediction(img):
+    img = np.array(img)/255
+    plt.imshow(img)
+    plt.show()
+    img = img.reshape(-1, 200, 200, 1)
+    isPneumonic = model.predict(img)[0]
+    print(isPneumonic)
+    if isPneumonic <0.15:
+        imgClass = "unknown or bad image"
+    elif isPneumonic<0.7:
+        imgClass = "Normal"
+    else:
+        imgClass = "pneumonia"
+    return imgClass
 
+def img_proess(img):
+    pil_image =img
+    
+    # Determine the side length for the square crop
+    side_length = min(pil_image.size)*1.4 #1.4 to make more usefull image
+    print(side_length)
+    
+    # Calculate cropping box for center cropping
+    left = (pil_image.width - side_length) //2
+    top = (pil_image.height - side_length) //2
+    right = (pil_image.width + side_length)//2
+    bottom = (pil_image.height + side_length) //2
+    
+    # Perform the center crop to make the image square
+    square_cropped_image = pil_image.crop((left, top, right, bottom))
+    final_image=square_cropped_image.resize((200,200),Image.BOX)
+    return final_image
+    
 # Set the title and header
-st.title("Image Processing with Streamlit")
+st.title("chest x-ray image classification for covid-19 detection for pneumonia")
 st.header("Upload an Image and Process It")
 
 # Upload image through Streamlit UI
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 
-def img_process(img):
-    #img = np.array(img) / 255
-    #img = img.reshape(-1, 200, 200, 1)
-    #img = img.reshape(200, 200)
-    #img = img.resize(200, 200)
-
-    return img
-
-def PneumoniaPrediction(img):
-   
-    isPneumonic = model.predict(img)[0]
-    print(isPneumonic)
-    if isPneumonic < 0.0001:
-        imgClass = "Unknown image"
-    elif isPneumonic < 0.5:
-        imgClass = "Normal"
-    else:
-        imgClass = "Pneumonic"
-    return imgClass
-
-
 # Process the uploaded image
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    #st.image(image, caption='Uploaded Image', use_column_width=True)
-    #resized_image = image.resize((200, 200))    
-    resized_image=img_process(image)
-    st.image(resized_image, caption='Uploaded Image', use_column_width=True)
-    #testing direct image
+    image_temp = Image.open(uploaded_file)
+    image=img_proess(image_temp)
+    st.image(image, caption='Uploaded Image after process', use_column_width=True)
     ans=PneumoniaPrediction(image)
    
 
@@ -59,4 +63,4 @@ if uploaded_file is not None:
     processed_image = image_array  # Placeholder for demonstration
     st.title(ans)
     # Display processed image
-    st.image(processed_image, caption='Processed Image', use_column_width=True)
+    #st.image(processed_image, caption='Processed Image', use_column_width=True)
